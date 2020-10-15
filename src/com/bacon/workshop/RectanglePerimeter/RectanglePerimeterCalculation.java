395381cc39cec77b04d7cab00bfeed6ef0f780d3
Janelyn <jancob@jpj.com>
2020-10-15 10:21:27 +0800

    Expression, Statements, Blocks exercise

--- src/com/bacon/workshop/RectanglePerimeter/RectanglePerimeterCalculation.java
+++ src/com/bacon/workshop/RectanglePerimeter/RectanglePerimeterCalculation.java
@@ -2,15 +2,15 @@ package com.bacon.workshop.RectanglePerimeter;
 
 public class RectanglePerimeterCalculation {
 
-    public void RectanglePerimeter(int rectangleWidth, int rectangleHeight) {
+    public void RectanglePerimeter(int rectangleHeight, int rectangleWidth) {
         int limit = 100;
-        int width = rectangleWidth;
         int height = rectangleHeight;
+        int width = rectangleWidth;
 
         int rectanglePerimeter = (width + height) * 2;
 
-        if (rectanglePerimeter < limit) {
-            System.out.println("The perimeter of height " + height + " and width " + width + " is greater than " + limit);
+        if (rectanglePerimeter > limit) {
+            System.out.println("The perimeter for height " + height + " and width " + width + " is greater than " + limit);
         }
     }
 
